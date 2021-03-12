f6efe1eb40e77533f07261a800e4e2f896e95731
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-03-12 18:51:57 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -1,6 +1,28 @@
-#include "header.h"
+#include<stdio.h>
+#include<stdlib.h>
 
-int iterative_fibo(int n)
+
+
+int fibo_iterative(int n)
 {
-    // write your iterative_fibo code here
+	int fn,f1,f2;
+	if(n==0||n==1)
+		return n;
+	else
+	{	for(int i=0;i<n;i++)
+		{
+			fn=f1+f2;
+			fn=f2;
+			f2=f1;
+			return fn;
+		}
+        }
+}
+
+int main()
+{
+	int n;
+	printf("donner la valeur de n:\n");
+	fibo_iterative(n);
+
 }
