bca59d46d985e421700c4f21465e9c101dcb7fe2
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-03-21 21:30:27 +0100

    Update primes_opt.c

--- TP1/EX3/primes_opt.c
+++ TP1/EX3/primes_opt.c
@@ -9,10 +9,25 @@
 static bool    is_prime_opt(int n)
 {
     // write is_prime function code here
+    bool x = {true};
+	if(n==1||n==2)
+		return true;	
+    for(int i=3;i<sqrt(n);i+=2)
+    {
+        if(n%i==0)
+            return false;   
+    }
+    return true;
 }
 
 
 void    get_primes_opt(int n)
 {
     // write get_primes_opt function code here that use is_prime_opt function
+    for (int i = 2;i<=n;i++)
+    {
+        if (is_prime_opt(i))
+            printf("%d is prime\n",i);
+    }
+    
 }
