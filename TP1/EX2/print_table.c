61ee9822068c8986b714c0a580e93f38d13c5503
ibtihal el banouri <80362007+ibtihal2002@users.noreply.github.com>
2021-06-30 20:33:07 +0100

    Update print_table.c

--- TP1/EX2/print_table.c
+++ TP1/EX2/print_table.c
@@ -6,4 +6,17 @@
 void    print_table(int *tab, int size)
 {
     // iterate the array tab and print its elements
+    
+    if(tab.head==NULL)
+    printf("[]");
+    else
+    {
+        size =tab.size;
+        printf("[");
+        for(int i=0;i < n-1;i++)
+            printf("%d,",tab.head[i]);
+        printf("%d]",tab.head[n-1]);
+    }
+    printf("\n");
+    return;
 }
